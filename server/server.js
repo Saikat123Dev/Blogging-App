10061541273fffa1501039410eb280d4bb0a6723
Saikat123Dev <berasaikat729@gmail.com>
2023-12-27 00:09:42 +0530

    added gitignore

--- server/server.js
+++ server/server.js
@@ -1,2 +1,7 @@
 import express from "express";
+import mongoose from 'mongoose';
 const server = express();
+let PORT = 3000;
+server.listen(PORT , ()=> {
+    console.log('listening on port --> '+ PORT)
+});
